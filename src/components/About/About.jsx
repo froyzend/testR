8e1f67d672f85afe7d52e15915bfbbf4575cfa30
Froyzend <froyzend1993@gmail.com>
2024-12-01 17:52:48 +0100

    add animation

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,37 +5,56 @@ const About = () => {
   return (
     <section className={css.aboutMe}>
       <div className={css.unicornInfo}>
-        <h1>Welcome to My Portfolio</h1>
-        <p>
-          &#34;My name is Kateryna, and I&apos;m passionate about technology and
-          personal development. Over the past few years, I&apos;ve transitioned
-          through several careers, which has helped me adapt to new challenges
-          and acquire valuable skills. Currently, I&apos;m growing in web
-          development and building a portfolio that showcases my progress and
-          ambitions.&#34;
-        </p>
+        <div className={css.textContainer}>
+          <h1 className={css.titleAbout}>Welcome to My Portfolio</h1>
+          <h2 className={css.unicornName}>
+            <strong> &#34;My name is Kateryna Folova</strong>,
+          </h2>
+          <p className={css.descriptionText}>
+            and I&apos;m passionate about technology and personal development.
+            Over the past few years, I&apos;ve transitioned through several
+            careers, which has helped me adapt to new challenges and acquire
+            valuable skills. Currently, I&apos;m growing in web development and
+            building a portfolio that showcases my progress and ambitions.
+            <span className={css.unicornName}>&#34;</span>
+          </p>
+        </div>
+        <img
+          className={css.unicorn}
+          src="https://cdn-icons-png.flaticon.com/512/3468/3468306.png"
+          alt="unicorn"
+          width="300"
+          height="200"
+        />
       </div>
-      <ul>
-        <li>
-          I have built and deployed several websites, including personal
-          projects and collaborations, using tools like React, JavaScript, and
-          CSS.
-        </li>
-        <li>
-          I am proficient in version control systems like Git – collaborating on
-          team projects and managing code efficiently is one of my strengths.
-        </li>
-        <li>
-          I have a deep understanding of web accessibility and always ensure the
-          websites I create are accessible to everyone.
-        </li>
-        <li>
-          I am always exploring new frameworks and libraries to broaden my
-          skills – currently learning [specific tech or framework].
-        </li>
-      </ul>
 
-      <Link to="/projects">View Projects</Link>
+      <div className={css.listContainer}>
+        <h2 className={css.listTitle}>My Strengths</h2>
+        <ul className={css.list}>
+          <li className={css.listItem}>
+            I have built and deployed several websites, including personal
+            projects and collaborations, using tools like React, JavaScript, and
+            CSS.
+          </li>
+          <li className={css.listItem}>
+            I am proficient in version control systems like Git – collaborating
+            on team projects and managing code efficiently is one of my
+            strengths.
+          </li>
+          <li className={css.listItem}>
+            I have a deep understanding of web accessibility and always ensure
+            the websites I create are accessible to everyone.
+          </li>
+          <li className={css.listItem}>
+            I am always exploring new frameworks and libraries to broaden my
+            skills – currently learning [specific tech or framework].
+          </li>
+        </ul>
+      </div>
+
+      <Link className={css.viewProjects} to="/projects">
+        View Projects
+      </Link>
     </section>
   );
 };
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,17 @@
 import { Link } from "react-router-dom";
 import NavList from "../NavList/NavList";
 import SocialIcons from "../NavList/SocialIcons";
-import css from "./Header.module.css";
-import unicorn from "../../asset/unicorn.svg";
+import css from "./Header.module.css"; /*
+import unicorn from "../../asset/unicorn.svg";*/
 
 const Header = () => {
   return (
     <header className={css.header}>
       <Link to="/" className={css.logo}>
-        <img src={unicorn} alt="unicorn" className={css.logoImage} />
+        <div className={css.planetIcon}>
+          <div className={css.ring}></div>
+          <div className={css.planet}></div>
+        </div>
       </Link>
       <NavList />
       <SocialIcons />
