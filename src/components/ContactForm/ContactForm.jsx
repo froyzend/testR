d9acf62cb3ba49cde6e4d2d338c6d3b2cac783bb
Froyzend <froyzend1993@gmail.com>
2024-12-01 18:25:33 +0100

    contact form save data

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -5,14 +5,14 @@ import css from "./ContactForm.module.css";
 const ContactForm = () => {
   const validationSchema = Yup.object({
     name: Yup.string().required("Name required"),
-    email: Yup.string()
-      .email(" email is invalid")
-      .required("Email is required"),
+    email: Yup.string().email("Email is invalid").required("Email is required"),
     message: Yup.string().required("Message required"),
   });
 
-  const handleSubmit = (values) => {
-    console.log("Form data:", values);
+  const handleSubmit = (values, { resetForm }) => {
+    const saveLocalStorage = JSON.stringify(values);
+    localStorage.setItem("formData", saveLocalStorage);
+    resetForm();
   };
 
   return (
@@ -21,60 +21,66 @@ const ContactForm = () => {
       validationSchema={validationSchema}
       onSubmit={handleSubmit}
     >
-      <Form className={css.FormContainer}>
-        <div className={css.formWrapper}>
-          <div className={css.field}>
-            <label htmlFor="name" className={css.label}>
-              Name
-            </label>
-            <Field
-              type="text"
-              id="name"
-              name="name"
-              placeholder="Your name"
-              className={css.inputField}
-            />
-            <ErrorMessage name="name" component="div" className={css.error} />
-          </div>
+      {() => (
+        <Form className={css.FormContainer}>
+          <div className={css.formWrapper}>
+            <div className={css.field}>
+              <label htmlFor="name" className={css.label}>
+                Name
+              </label>
+              <Field
+                type="text"
+                id="name"
+                name="name"
+                placeholder="Your name"
+                className={css.inputField}
+              />
+              <ErrorMessage name="name" component="div" className={css.error} />
+            </div>
 
-          <div className={css.field}>
-            <label htmlFor="email" className={css.label}>
-              Email
-            </label>
-            <Field
-              type="email"
-              id="email"
-              name="email"
-              placeholder=" Your email"
-              className={css.inputField}
-            />
-            <ErrorMessage name="email" component="div" className={css.error} />
-          </div>
+            <div className={css.field}>
+              <label htmlFor="email" className={css.label}>
+                Email
+              </label>
+              <Field
+                type="email"
+                id="email"
+                name="email"
+                placeholder="Your email"
+                className={css.inputField}
+              />
+              <ErrorMessage
+                name="email"
+                component="div"
+                className={css.error}
+              />
+            </div>
 
-          <div className={css.field}>
-            <label htmlFor="message" className={css.label}>
-              Message
-            </label>
-            <Field
-              as="textarea"
-              id="message"
-              name="message"
-              placeholder="Your message"
-              rows="4"
-              className={css.textareaField}
-            />
-            <ErrorMessage
-              name="message"
-              component="div"
-              className={css.error}
-            />
-          </div>
+            <div className={css.field}>
+              <label htmlFor="message" className={css.label}>
+                Message
+              </label>
+              <Field
+                as="textarea"
+                id="message"
+                name="message"
+                placeholder="Your message"
+                rows="4"
+                className={css.textareaField}
+              />
+              <ErrorMessage
+                name="message"
+                component="div"
+                className={css.error}
+              />
+            </div>
 
-          <button type="submit" className={css.button}>
-            Submit
-          </button>
-        </div>
-      </Form>
+            <button type="submit" className={css.button}>
+              Submit
+            </button>
+          </div>
+        </Form>
+      )}
     </Formik>
   );
 };
